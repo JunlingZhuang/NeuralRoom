95275c3eebe4d494a056ffca398d98eb34f711d2
ZHUANG JUNLING <108850976+JunlingZhuang@users.noreply.github.com>
2024-05-11 21:16:57 -0400

    update test

--- api/index.py
+++ api/index.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+
+global_count = 0  # 全局变量来存储计数
 
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-
 @app.route("/api/test", methods=["GET"])
-def addCount(count=1):
-    count += 1
-    return {"count": count}
+def add_count():
+    global global_count
+    global_count += 1  # 每次调用时增加全局计数
+    return jsonify({"count": global_count})
+
+
+if __name__ == "__main__":
+    app.run()
