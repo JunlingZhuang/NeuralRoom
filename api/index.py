a0f10ff4a3304cd6fdfcff93d30c1a64a17f6b75
ZHUANG JUNLING <108850976+JunlingZhuang@users.noreply.github.com>
2024-05-30 20:08:46 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ global_count = 0
 
 @app.route("/api/python")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World!!</p>"
 
 @app.route("/api/test", methods=["GET"])
 def add_count():
